11b4899ad0ad2dfcf0957476dfcc3aac2a56badb
Anton Markov <amrkov5@gmail.com>
2024-01-20 16:28:17 +0200

    fix: fixed task 02

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -9,21 +9,29 @@ const writeStream = fs.createWriteStream(pathToFile, { flags: 'a' });
 rl.setPrompt('Greetings!\nEnter text: ');
 rl.prompt();
 rl.on('line', (input) => {
-  if (input.length === 0) {
-    console.log('Nothing has been entered...');
-    process.stdout.write('Enter text: ');
+  if (input === 'exit' || input === 'Exit') {
+    stopApp();
   } else {
-    process.stdout.write('Enter text: ');
-    writeStream.write(`${input}\n`);
-    rl.on('error', (error) => console.log('Error', error.message));
+    if (input.length === 0) {
+      console.log('Nothing has been entered...');
+      process.stdout.write('Enter text: ');
+    } else {
+      process.stdout.write('Enter text: ');
+      writeStream.write(`${input}\n`);
+      rl.on('error', (error) => console.log('Error', error.message));
+    }
   }
 });
 
-rl.on('SIGINT', () => {
-  console.log('\nGood bye!');
-  writeStream.close();
+function stopApp() {
   rl.close();
+  writeStream.close();
+  console.log('\nGood bye!');
   setTimeout(() => {
     process.exit();
   }, 500);
+}
+
+rl.on('SIGINT', () => {
+  stopApp();
 });
